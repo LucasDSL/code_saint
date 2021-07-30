b0ab419efb986ded7012cc7cb8157fadf53f965a
LucasDSL <lucasdslima2002@gmail.com>
2021-07-30 13:47:56 -0300

    primeira parte quadrado mágico

--- CodCad-Programação-Básica/quadrado_magico.cpp
+++ CodCad-Programação-Básica/quadrado_magico.cpp
@@ -1,37 +1,23 @@
 #include<iostream>
 using namespace std;
-bool verificalinhas(int dimensoes)
-{
-    int soma_anterior = 0, soma_atual;
-    for(int i = 0; i < dimensoes; i++){
-
-        soma_anterior = soma_atual;
-        
-        soma_atual = 0;
-        
-        for(int j = 0; j < dimensoes; j++){
-            soma_atual += matriz[i][j];
-        }
-        
-        if(i != 0 && soma_atual != soma_anterior){
-            return false;
-        }
-    }
-    return true;
-}
-
 int main(){
     int dimensoes;
     cin >> dimensoes;
     int matriz[dimensoes][dimensoes];
 
-
+    int lin_atual = 0, diag_atual = 0, col_atual = 0;
+    int lin_antiga = 0, diag_antiga = 0, col_antiga = 0;
     for(int i = 0; i < dimensoes; i++){
+        lin_atual = 0;
         for(int j = 0; j < dimensoes; j++){
             cin >> matriz[i][j];
+            lin_atual += matriz[i][j];
         }
+        if (lin_antiga != lin_atual && i != 0){
+            cout << -1 << endl;
+            break;
+        }
+        lin_antiga = lin_atual;
     }
-
-    bool linhas = verificalinhas(dimensoes);
-    
+    // Verificação de linhas pronta 
 }
